8c31cfdb09820abbe57cb755f3e9e6397973b9c0
Jon Robison <narfman0@yahoo.com>
2014-03-08 17:19:30 -0500

    -Add current a2/a3 work

--- a2/UMChatClient.py
+++ a2/UMChatClient.py
@@ -1,13 +1,9 @@
 #!/bin/python
-import select,socket,struct,sys,traceback
+import getpass, os, signal, socket, struct, sys, time
 
 DEFAULT_HOST=socket.gethostname()
 DEFAULT_PORT=10009
-
-def getClientSocket(host,port):
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.connect((host, port))
-  return s
+DEFAULT_TYPE='u'
 
 def startMulticastReceiver(group, port):
   recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -20,58 +16,83 @@ def startMulticastReceiver(group, port):
   send_sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
   return (recv_sock,send_sock)
 
-def startClient(host,port):
-  s = getClientSocket(host,port)
-  recv_sock = None
-  send_sock = None
-  mcast_group = None
-  mcast_port = None
-  socket_list = [sys.stdin, s]
+def printSocket(host,port):
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  s.bind((host,port))
   while True:
-    read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
-    for sock in read_sockets:
-      #incoming message from remote server
-      if sock == s:
-        data = sock.recv(1024)
-        if not data :
-          print 'Disconnected from server'
-          sock.close()
-          sys.exit()
-        else :
-          if '|' in data and recv_sock is None:
-            mcast_group, mcast_port_str = data.split('|')
-            mcast_port = int(mcast_port_str)
-            print('Received multicast group info ip: ' + mcast_group + ' port: ' + mcast_port_str)
-            try:
-              recv_sock,send_sock = startMulticastReceiver(mcast_group, mcast_port)
-              socket_list.append(recv_sock)
-            except:
-              print('Error connecting to multicast group')
-              traceback.print_exc()
-              sys.exit()
-      elif sock == sys.stdin:
-        msg = sys.stdin.readline()
-        if msg != '':
-          send_sock.sendto(msg, (mcast_group, mcast_port))
-        else:
-          print('No chars or ctrl-d pressed, quitting')
-          sys.exit()
-      elif sock == recv_sock:
-        data,addr = sock.recvfrom(1024)
-        if not data or data == '':
-          sock.close()
-          print('Error, data null or empty, disconnected from multicast group')
-          sys.exit()
-        else:
-          print(str(addr) + ': ' + data) 
+    try:
+      (data, address) = s.recvfrom(1024)
+      print(str(address) + ": " + data)
+    except:
+      pass
+
+def mcastClient(s, host, port):
+  s.send('0') # sends "0" & receives M, P, L and E.
+  m = s.recv(32).strip()
+  p = int(s.recv(5))
+  l = int(s.recv(7))
+  e = int(s.recv(7))
+  print('Connected with m=' + m + ' p=' + str(p) + ' l='+ str(l) + ' e=' + str(e))
+  _recv_sock, _send_sock = startMulticastReceiver(m, p)
+
+def unicastClient(s, host, port):
+  s.send('1') # sends "1" & receives P, L and E.
+  p = int(s.recv(5))
+  l = int(s.recv(7))
+  e = int(s.recv(7))
+  s.settimeout(1)
+  print('Connected with p=' + str(p) + ' l='+ str(l) + ' e=' + str(e))
+  listPrinter=os.fork()
+  socketPrinter=os.fork()
+  if(listPrinter == 0):
+    while True:
+      try:
+        data = s.recv(1024)
+        if data == '':
+          break
+        print('Printing LIST:\n' + data)
+      except:
+        pass
+  elif(socketPrinter == 0):
+    printSocket(host, port)
+  else:
+    signal.signal(signal.SIGINT, lambda signum,frame: s.send(str(l)))#ctrl-c
+    signal.signal(signal.SIGQUIT, lambda signum,frame: s.send(str(e)))#ctrl-/
+    us=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while True:
+      msg = sys.stdin.readline().strip()
+      if msg != '':
+        us.sendto(msg,(host,p))
+      else:
+        print('No chars or ctrl-d pressed, quitting')
+        us.close()
+        s.send(str(e))
+        s.close()
+        os.kill(listPrinter, signal.SIGKILL)
+        os.kill(socketPrinter, signal.SIGKILL)
+        sys.exit()
+    
+
+def startClient(host,port,socketType):
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.connect((host, port))
+  s.send(getpass.getuser()) # send username
+  time.sleep(1)
+  if socketType == 'm':
+    mcastClient(s, host, port)
+  else:
+    unicastClient(s, host, port)
 
 if __name__ == "__main__":
-  if len(sys.argv) != 3:
-    print('No host given as argument 1, defaulting to ' + DEFAULT_HOST)
-    print('No port given as argument 2, defaulting to ' + str(DEFAULT_PORT))
+  if len(sys.argv) != 4:
+    print('No <shost> given as argument 1, defaulting to ' + DEFAULT_HOST)
+    print('No <sport> given as argument 2, defaulting to ' + str(DEFAULT_PORT))
+    print('No <u|m> given as argument 3, defaulting to ' + DEFAULT_TYPE)
     host=DEFAULT_HOST
     port=DEFAULT_PORT
+    socketType=DEFAULT_TYPE
   else:
     host=sys.argv[1]
     port=int(sys.argv[2])
-  startClient(host,port)
+    socketType=sys.argv[3]
+  startClient(host,port,socketType)
---
+++ a3/UMChatServer.py
@@ -0,0 +1,98 @@
+#!/bin/python
+import select, signal, socket, sys, time
+from random import randint
+
+DEFAULT_PORT=10010
+
+def generateMulticastGroupPort():
+  return randint(9999,11001)
+
+def generateMulticastGroupIP():
+  return str(randint(224,239)) + '.' + str(randint(0,255)) + '.' + str(randint(0,255)) + '.' + str(randint(0,255))
+
+def generateNumber():
+  return randint(1000000,9999999)
+
+def printConnected(connected):
+  print('Connected clients:')
+  for user,address in connected.items():
+    print('User: ' + user + ' address: ' + str(address))
+
+def startServer(port):
+  l=generateNumber()
+  e=generateNumber()
+  p=generateMulticastGroupPort()
+  m=generateMulticastGroupIP()
+  print "Using l=" + str(l) + ' e=' + str(e) + ' p=' + str(p) + ' m=' + str(m)
+  
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.bind((socket.gethostname(), port))
+  s.listen(1)
+  u = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  u.bind((socket.gethostname(), p))
+  print "Bound to %s" % str(socket.gethostname())
+  
+  connectedClients={}
+  signal.signal(signal.SIGINT, lambda signum,frame: printConnected(connectedClients))
+  socket_list = [sys.stdin, s, u]
+  while True:
+    try:
+      read_sockets,_write_sockets,_error_sockets = select.select(socket_list,[],[])
+      for sock in read_sockets:
+        #New connection
+        if sock == s:
+            # Handle the case in which there is a new connection recieved through server_socket
+            sockfd, _addr = s.accept()
+            username=sockfd.recv(1024)
+            connectedClients[sockfd.getpeername()]=username
+            print('New TCP Client: ' + str(sockfd.getpeername()) + '[' + username + ']')
+            _type=sockfd.recv(1)
+            sockfd.send(str(p))
+            time.sleep(1)
+            sockfd.send(str(l))
+            time.sleep(1)
+            sockfd.send(str(e))
+            time.sleep(1)
+        elif sock == u:
+            (data,address) = u.recvfrom(1024)
+            if address not in connectedClients:
+              connectedClients[address]=username
+              print('New UDP Client: ' + str(address) + '[' + username + ']')
+              _type=sockfd.recv(1)
+              sockfd.send(str(m))
+              time.sleep(1)
+              sockfd.send(str(p))
+              time.sleep(1)
+              sockfd.send(str(l))
+              time.sleep(1)
+              sockfd.send(str(e))
+              time.sleep(1)
+            else:
+              print('Forward message to multicast group. Msg: ' + data)
+        #Some incoming message from a client
+        else:
+            # Data received from client, process it
+            try:
+                #In Windows, sometimes when a TCP program closes abruptly,
+                # a "Connection reset by peer" exception will be thrown
+                data = sock.recv(1024) #doesn't block
+                if data is None or data == '':
+                  print "Client (%s, %s) disconnected" % sock.getpeername()
+                  del connectedClients[sock.getpeername()]
+                  sock.close()
+            except:
+                print "Client (%s, %s) is offline" % sock.getpeername()
+                del connectedClients[sock.getpeername()]
+                sock.close()
+                continue
+    except:
+      #Ctrl-c perhaps, just pass
+      pass
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print('No port given as argument 1, defaulting to ' + str(DEFAULT_PORT))
+    port=DEFAULT_PORT
+  else:
+    port=int(sys.argv[1])
+  startServer(port)
